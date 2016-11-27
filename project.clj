0dc10cfc6f229750d7f01498fc4b929df77b73d9
Mikaela Patella <m.c.patella@gmail.com>
2016-11-27 00:19:03 -0500

    bump clj to 1.9-alpha14 for spec and add test.check

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [org.clojure/core.async "0.2.374"]
-                 [com.taoensso/timbre "4.3.1"]])
+                 [org.clojure/test.check "0.9.0"]
+                 [com.taoensso/timbre "4.3.1"]
+                 [prismatic/schema "1.1.3"]])
