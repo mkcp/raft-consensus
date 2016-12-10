dc9485c714c99261c9a7ffb5c8ca01b3d862e60f
Mikaela Patella <m.c.patella@gmail.com>
2016-12-10 13:01:00 -0800

    sketch out the log functions

--- src/raft/log.clj
+++ src/raft/log.clj
@@ -4,3 +4,39 @@
 ;; What index we're on: commit-index?
 ;; A value called match-index is showing up in AppendEntries responses
 ;; The requests contain the previous-index and the commit-index, which I believe are the same if they haven't been updated recently.
+
+(defn add [log entry]
+  (conj log entry))
+
+(defn commit [log entry]
+  (comment (state-change {:state log
+                          :compare :uncommitted
+                          :swap :committed})))
+
+
+;; If this is append only then removing an entry probably isn't the way to go.
+;; Log compaction, that is, truncating under a certain index, that makes sense.
+;;;;;;;;;;;; TODO
+(defn match?
+  [entry entry2]
+  (= (:id entry)
+     (:id entry2)))
+
+(defn remove [log entry]
+  (filter #(match entry %) log))
+
+(defn remove-test []
+  (testing "filters matching entry from log"
+    (let [log-pre [{:id :meow
+                    :entry [:append-entries {}]}
+                   {:id :bark
+                    :entry [:append-entries {}]}]
+
+          log-post [{:id :bark
+                     :entry [:append-entries {}]}]]
+
+      (is (= (remove log-pre :meow)
+             log-post)))))
+
+(defn cas [log entry])
+;;;;;;;;;;;;; end TODO
