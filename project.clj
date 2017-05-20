98015c23b5ddfed63e72f89f6f6ab3ab0a82af07
Mikaela Patella <m.c.patella@gmail.com>
2017-05-20 15:31:21 -0700

    add main to project.clj and clean it up

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject raft "0.1.0-SNAPSHOT"
-  :description "Raft is a distributed consensus algorithm optimized for understanding."
+  :description "Toy raft implementation for use with maelstrom"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main raft.core
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [org.clojure/core.async "0.2.374"]
-                 [org.clojure/tools.cli "0.3.5"]])
+                 [org.clojure/core.async "0.2.374"]])
